ea28f065d431b9d65ab6f94f6946a90af18f8b34
EvgeniyLednev <lednev.fortech@gmail.com>
2025-01-17 07:03:09 +0300

    - Добавлен метод кэширования игрока

--- src/modules/server-discovery/server-discovery-cache.service.ts
+++ src/modules/server-discovery/server-discovery-cache.service.ts
@@ -5,6 +5,7 @@ import { Cache } from 'cache-manager';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { ServerDiscoveryService } from './server-discovery.service';
 import { GetCachedPlayerDto } from './dto/get-cached-player.dto';
+import { Address } from 'src/entities/address.entity';
 
 @Injectable()
 export class ServerDiscoveryCacheService {
@@ -19,13 +20,19 @@ export class ServerDiscoveryCacheService {
 
   async getCachedPlayer(dto: GetCachedPlayerDto): Promise<Player | null> {
     const { host, port, username } = dto;
-    const player = await this.cacheManager.get<Player>(
-      `${host}:${port}:${username}`,
-    );
+    const key = `${host}:${port}:${username}`;
+    const player = await this.cacheManager.get<Player>(key);
     return player || null;
   }
 
-  @Cron(CronExpression.EVERY_10_SECONDS)
+  async сachePlayer(address: Address, player: Player): Promise<void> {
+    const { host, port } = address;
+    const { name } = player;
+    const key = `${host}:${port}:${name}`;
+    await this.cacheManager.set<Player>(key, player, 60000); // Минута
+  }
+
+  @Cron(CronExpression.EVERY_30_SECONDS)
   protected async cachePlayers() {
     const servers = await this.serverDiscoveryService.getServersWithPlayers();
 
@@ -33,7 +40,7 @@ export class ServerDiscoveryCacheService {
       for (const player of server.players) {
         const { host, port } = server.server.address;
         const key = `${host}:${port}:${player.name}`;
-        await this.cacheManager.set(key, player, 5000); // 5 секунд
+        await this.cacheManager.set(key, player, 60000); // Минута
       }
     }
     this.logger.debug('Игроки кэшированы');
