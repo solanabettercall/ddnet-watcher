dc699a725e344cbb0f79f6e57fcd63f5af7fb136
EvgeniyLednev <lednev.fortech@gmail.com>
2025-01-26 23:56:54 +0300

    - enableShutdownHooks

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableShutdownHooks();
   await app.listen(process.env.APP_PORT ?? 3000);
 }
 bootstrap();
